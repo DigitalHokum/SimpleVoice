0a24c24a39c551b9a06f002f36418b8fb8aec23a
Matt <contact@maleero.com>
2020-04-11 00:14:33 -0500

    VoiceHandlerRelay test fixes

--- SimpleVoice.Test/RequestHandlerTest.cs
+++ SimpleVoice.Test/RequestHandlerTest.cs
@@ -3,6 +3,7 @@ using Amazon.Lambda.TestUtilities;
 using SimpleVoice.Entry;
 using Xunit;
 using SimpleVoice.Platforms.Alexa;
+using SimpleVoice.Platforms.Alexa.Data.Request;
 
 namespace SimpleVoice.Test
 {
@@ -24,13 +25,24 @@ namespace SimpleVoice.Test
         public async Task VoiceHandlerRelay()
         {
             string speechTextToRelay = "Relay this!";
+            string repromptTextToRelay = "Also, Relay this!";
             AlexaRequest request = MockData.AlexaRequest("RelayIntent");
-            
+            request.Request.Intent.Slots.Add("speech", new Slot()
+            {
+                Name = "speech",
+                Value = speechTextToRelay
+            });
+            request.Request.Intent.Slots.Add("reprompt", new Slot()
+            {
+                Name = "reprompt",
+                Value = repromptTextToRelay
+            });
+
             Lambda lambda = new Lambda();
             AlexaResponse response = (AlexaResponse) lambda.Handle(request, new TestLambdaContext());
 
-            Assert.Equal("TestIntent Speech", response.Speech);
-            Assert.Equal("TestIntent Reprompt", response.Reprompt);
+            Assert.Equal(speechTextToRelay, response.Speech);
+            Assert.Equal(repromptTextToRelay, response.Reprompt);
         }
     }
 }
